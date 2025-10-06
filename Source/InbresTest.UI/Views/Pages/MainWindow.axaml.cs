cf12f3dfe52de5100b002988c5f6a55107a8fe78
Lukus12 <109617504+Lukus12@users.noreply.github.com>
2025-10-06 16:48:55 +0300

    Создана начальная файловая структура

---
+++ Source/InbresTest.UI/Views/Pages/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace InbresTest.Views.Pages;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
